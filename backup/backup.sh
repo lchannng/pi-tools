7bcb1802eba4e2622fd44d9246946597c3d15ae0
lchannng <l.channng@gmail.com>
2021-10-06 11:27:03 +0800

    update backup scripts

--- backup/backup.sh
+++ backup/backup.sh
@@ -7,7 +7,7 @@ backup_root=~/pi-backup
 backup_dir=`date '+%Y-%m-%d'`
 backup_path=${backup_root}/${backup_dir}/
 mkdir -p ${backup_path}
-rsync -avP --delete --files-from=./files.txt / ${backup_path}
+rsync -arvP --delete --files-from=./files.txt / ${backup_path}
 pushd ${backup_root}
 ln -snf ${backup_dir} latest
 tar zcvf pi-latest.tar.gz ${backup_dir}
